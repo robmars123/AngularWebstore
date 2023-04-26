dd16c35703517409abf999f9e200df470a96da8b
robmars123 <robmars2008@gmail.com>
2023-04-26 18:31:34 -0500

    Added Unit Testing Project and Mock data for testing.

---
+++ ViewModels/CreateProductModel.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Models;
+
+namespace ReactWebstore.ViewModels
+{
+    public class CreateProductModel
+    {
+        public Product Product { get; set; }
+        public List<Product> productList { get; set; }
+        public ProductImage PrimaryProduct_Image { get; set; }
+        public List<ProductImage> productImagesList { get; set; }
+
+        public virtual ProductImage productImage { get; set; }
+
+        public virtual Category Categories { get; set; }
+
+        public virtual Subcategory Subcategories { get; set; }
+    }
+}
